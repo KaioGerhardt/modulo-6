49c0d95da3a3a21b9538555e5b2c02beb902b458
Kaio Gerhardt <gerhardt.kaio@gmail.com>
2022-06-03 22:56:17 -0300

    commit

---  Desafio_M6_Kaio_Henriqe_Gerhardt_Fernandes/src/MEC.java
+++  Desafio_M6_Kaio_Henriqe_Gerhardt_Fernandes/src/MEC.java
@@ -2,10 +2,17 @@
 public class MEC {
 	
 	public void imprimeUniversidades(Universidade[] uni) {
-		int i = 0;
-		while(i < uni.length) {//laço para passar por cada indice do array
-			System.out.println("*** Dados da "+ i + "° Universidade ***");
+		for(int i = 0; i < uni.length; i++) {
 			
+		}
+		
+		for (int i = 0; i < uni.length; i++) {//laço para passar por cada indice do array
+			System.out.println("*** Dados da "+ i + "° Universidade ***");
+			uni[i].imprimeInfo();
+			
+			if(uni instanceof Privada) {
+				System.out.println("Tipo: Privada");
+			}//else if(uni instanceof Publica)
 		}
 	}
 }
---  Desafio_M6_Kaio_Henriqe_Gerhardt_Fernandes/src/Principal.java
+++  Desafio_M6_Kaio_Henriqe_Gerhardt_Fernandes/src/Principal.java
@@ -2,5 +2,16 @@
 public class Principal {
 	public static void main(String[] args) {
 		// criação do array e import do numero de elementos
+		int quantidade = Teclado.leInt("Digite a quantidade de elementos do Array: ");
+		Universidade[] unive = new Universidade[quantidade];
+		//unive[1] = {5};
+		
+		//laco de repeticao para a criação das universidades
+		for(int i = 0; i < unive.length; i++) {
+			
+			//teste
+			
+			
+		}
 	}
 }
