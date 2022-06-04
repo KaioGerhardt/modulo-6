850178379eec617bed71ff94fd94b68c8d5257af
Kaio Gerhardt <gerhardt.kaio@gmail.com>
2022-06-03 21:50:00 -0300

    commit

---  Desafio_M6_Kaio_Henriqe_Gerhardt_Fernandes/src/MEC.java
+++  Desafio_M6_Kaio_Henriqe_Gerhardt_Fernandes/src/MEC.java
@@ -2,8 +2,9 @@
 public class MEC {
 	
 	public void imprimeUniversidades(Universidade[] uni) {
-		//public int i;
-		for(i=0, i < uni.length, i++) {
+		int i = 0;
+		while(i < uni.length) {//laço para passar por cada indice do array
+			System.out.println("*** Dados da "+ i + "° Universidade ***");
 			
 		}
 	}
---
+++  Desafio_M6_Kaio_Henriqe_Gerhardt_Fernandes/src/Principal.java
@@ -0,0 +1,6 @@
+
+public class Principal {
+	public static void main(String[] args) {
+		// criação do array e import do numero de elementos
+	}
+}
